fd42f29c738030737fddfbf660bb9f52f59d521c
jammatth <jrm1184@gmail.com>
2016-03-25 14:27:21 -0500

    Debug.

--- Python_Starter_Code/Switch_Example.py
+++ Python_Starter_Code/Switch_Example.py
@@ -6,9 +6,9 @@ import os
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12,GPIO.IN,pull_up_down=GPIO.PUD_UP)
-# GPIO.setup(40, GPIO.OUT)
-# GPIO.setup(38, GPIO.OUT)
-# GPIO.setup(36, GPIO.OUT)
+GPIO.setup(40, GPIO.OUT)
+GPIO.setup(38, GPIO.OUT)
+GPIO.setup(36, GPIO.OUT)
 
 def Start_Code():
     running = 0
@@ -19,15 +19,30 @@ def Start_Code():
             blink_proc = subprocess.Popen("/home/pi/Self_Driving_Car_Project/SDRC_Project/SDRC_Project/Python_Starter_Code/Blink_Tri.sh", shell=True, preexec_fn=os.setsid)
             time.sleep(0.2)
             running = 1
-        elif input_state == True and running == 1:
-            print('Off')
-            os.killpg(blink_proc.pid, signal.SIGTERM)
-            blink_proc = None
-            GPIO.cleanup()
-            GPIO.setmode(GPIO.BOARD)
-            GPIO.setup(12,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+        while input_state == True:
+
+            GPIO.output(40,True)
             time.sleep(0.2)
-            running = 0
+            GPIO.output(40,False)
+            time.sleep(0.2)
+
+            if running == 1:
+                print('Off')
+                os.killpg(blink_proc.pid, signal.SIGTERM)
+                blink_proc = None
+
+                GPIO.output(40,False)
+                GPIO.output(38,False)
+                GPIO.output(36,False)
+
+                GPIO.cleanup()
+                GPIO.setmode(GPIO.BOARD)
+
+                GPIO.setup(12,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+                GPIO.setup(40, GPIO.OUT)
+                running = 0
+
+
     return;
 
 Start_Code()
