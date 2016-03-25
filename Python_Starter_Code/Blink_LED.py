9588d8947e41bdc6391b2c10c5bda4d7646bf404
jammatth <jrm1184@gmail.com>
2016-03-25 14:36:47 -0500

    Debug.

--- Python_Starter_Code/Blink_LED.py
+++ Python_Starter_Code/Blink_LED.py
@@ -7,21 +7,19 @@ GPIO.setup(38, GPIO.OUT)
 GPIO.setup(36, GPIO.OUT)
 
 def Blink_Tri():
-        x = 1
-        for x in range (1,10):
-            GPIO.output(38,True)
-            time.sleep(0.2)
-            GPIO.output(38,False)
-            time.sleep(0.2)
-            x += 1
-        x = 1
-        for x in range (1,10):
-            GPIO.output(36,True)
-            time.sleep(0.2)
-            x += 1
-
+    x = 1
+    for x in range (1,10):
+        GPIO.output(38,True)
+        time.sleep(0.2)
+        GPIO.output(38,False)
+        time.sleep(0.2)
+        x += 1
     GPIO.output(38,False)
-    GPIO.output(36,False)
+    while True:
+        PIO.output(36,True)
+        time.sleep(0.02)
+        GPIO.output(36,False)
+        time.sleep(0.02)
 
     return;
 
