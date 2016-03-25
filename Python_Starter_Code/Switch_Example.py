97531377968487436d77f2f3112e6c866bf6e2c7
jammatth <jrm1184@gmail.com>
2016-03-25 11:52:03 -0500

    Tab error fixed.

--- Python_Starter_Code/Switch_Example.py
+++ Python_Starter_Code/Switch_Example.py
@@ -11,10 +11,10 @@ GPIO.setup(12,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 while True:
     input_state = GPIO.input(12)
     if input_state == False:
-            print('On')
-            time.sleep(0.2)
-            process = Popen(["Blink", "Blink_LED.py"])
-        elif input_state == True:
-            print('Off')
-            time.sleep(0.2)
-            process = Popen.kill(["Blink", "Blink_LED.py"])
+        print('On')
+        time.sleep(0.2)
+        process = Popen(["Blink", "Blink_LED.py"])
+    elif input_state == True:
+        print('Off')
+        time.sleep(0.2)
+        process = Popen.kill(["Blink", "Blink_LED.py"])
