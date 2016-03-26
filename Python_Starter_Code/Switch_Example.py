a10eff50a1bee4d326fac02283854fc40ea535a4
jammatth <jrm1184@gmail.com>
2016-03-26 08:56:09 -0500

    Debug.

--- Python_Starter_Code/Switch_Example.py
+++ Python_Starter_Code/Switch_Example.py
@@ -24,17 +24,17 @@ def Start_Code():
             input_state = GPIO.input(12)
             GPIO.output(40,True)
             time.sleep(0.2)
+            nput_state = GPIO.input(12)
             GPIO.output(40,False)
             time.sleep(0.2)
 
             if running == 1:
-                print('Off')
-                os.killpg(blink_proc.pid, signal.SIGTERM)
-                blink_proc = None
-
                 GPIO.output(40,False)
                 GPIO.output(38,False)
                 GPIO.output(36,False)
+                print('Off')
+                os.killpg(blink_proc.pid, signal.SIGTERM)
+                blink_proc = None
 
                 GPIO.cleanup()
                 GPIO.setmode(GPIO.BOARD)
@@ -55,5 +55,3 @@ try:
     Start_Code()
 except KeyboardInterrupt:
     GPIO.cleanup()
-
-GPIO.cleanup()
