a82076690dd1c933e7c7ef86b55c809d021c3daa
jammatth <jrm1184@gmail.com>
2016-03-28 14:47:02 -0500

    Small changes.  Pin 18 is the PWM pin for rpi.

--- Python_Starter_Code/PWM_Example.py
+++ Python_Starter_Code/PWM_Example.py
@@ -3,9 +3,10 @@ import time
 
 GPIO.setmode(GPIO.BOARD) # select the GPIO numbering scheme to use.
 
-pwm_obj = GPIO.PWM(13, 400) #Sets pin 13 as a PWM output at 400 Hz.
+pwm_obj = GPIO.PWM(18, 50) #Sets pin 13 as a PWM output at 50 Hz.
 
 pwm_obj.start(100) #Starts the PWM with a duty cycle of 100 (Duty cycle ranges between 0 and 10).
 
 pwm_obj.ChangeDutyCycle(50) #Changes the Duty cycle to 50 (obviously).
-pw
+
+# Use ChangeDutyCycle() to control the position of the servo in pwm mode.
