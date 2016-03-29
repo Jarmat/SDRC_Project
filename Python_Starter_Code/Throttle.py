cb59eeb12bec122630ff20ab5f60722e672095e5
jammatth <jrm1184@gmail.com>
2016-03-28 20:37:15 -0500

    Debug

--- Python_Starter_Code/Throttle.py
+++ Python_Starter_Code/Throttle.py
@@ -1,31 +1,17 @@
 import RPi.GPIO as GPIO
 import time
 
-<<<<<<< HEAD
-# def init():
+#def initialize():
 GPIO.setmode(GPIO.BOARD) # select the GPIO numbering scheme to use.
 GPIO.setup(29, GPIO.OUT)
 GPIO.setup(31, GPIO.OUT)
 GPIO.setup(33, GPIO.OUT)
-pwm_29 = GPIO.PWM(29, 100)
-pwm_31 = GPIO.PWM(31, 100)
-pwm_29.start(0)
-pwm_31.start(0)
+pwm29 = GPIO.PWM(29, 100)
+pwm31 = GPIO.PWM(31, 100)
+pwm29.start(0)
+pwm31.start(0)
 GPIO.output(33, True)
-    # return;
-=======
-#def initialize():
-    GPIO.setmode(GPIO.BOARD) # select the GPIO numbering scheme to use.
-    GPIO.setup(29, GPIO.OUT)
-    GPIO.setup(31, GPIO.OUT)
-    GPIO.setup(33, GPIO.OUT)
-    pwm29 = GPIO.PWM(29, 100)
-    pwm31 = GPIO.PWM(31, 100)
-    pwm29.start(0)
-    pwm31.start(0)
-    GPIO.output(33, True)
  #   return;
->>>>>>> b77419cc290d9ff26cb90b10e1065e7f89032f84
 
 def Forward(ts,velocity):
     pwm31.ChangeDutyCycle(velocity)
@@ -51,13 +37,6 @@ def Clean():
     GPIO.cleanup()
     return;
 
-
-
-<<<<<<< HEAD
-# init()
-=======
-#initialize()
->>>>>>> b77419cc290d9ff26cb90b10e1065e7f89032f84
 
 for x in range (5):
     Forward(0.2,20)
