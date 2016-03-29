b77419cc290d9ff26cb90b10e1065e7f89032f84
James <jrm1184@gmail.com>
2016-03-29 01:33:49 +0000

    Debug

--- Python_Starter_Code/Throttle.py
+++ Python_Starter_Code/Throttle.py
@@ -1,45 +1,45 @@
 import RPi.GPIO as GPIO
 import time
 
-def init():
+#def initialize():
     GPIO.setmode(GPIO.BOARD) # select the GPIO numbering scheme to use.
     GPIO.setup(29, GPIO.OUT)
     GPIO.setup(31, GPIO.OUT)
     GPIO.setup(33, GPIO.OUT)
-    pwm_29 = GPIO.PWM(29, 100)
-    pwm_31 = GPIO.PWM(31, 100)
-    pwm_29.start(0)
-    pwm_31.start(0)
+    pwm29 = GPIO.PWM(29, 100)
+    pwm31 = GPIO.PWM(31, 100)
+    pwm29.start(0)
+    pwm31.start(0)
     GPIO.output(33, True)
-    return;
+ #   return;
 
 def Forward(ts,velocity):
-    pwm_31.ChangeDutyCycle(velocity)
+    pwm31.ChangeDutyCycle(velocity)
     time.sleep(ts)
     return;
 
 
 
 def Reverse(ts,velocity):
-    pwm_29.ChangeDutyCycle(velocity)
+    pwm29.ChangeDutyCycle(velocity)
     time.sleep(ts)
     return;
 
 def Stop(ts):
     GPIO.output(33, False)
-    pwm_29.ChangeDutyCycle(0)
-    pwm_31.ChangeDutyCycle(0)
+    pwm29.ChangeDutyCycle(0)
+    pwm31.ChangeDutyCycle(0)
     return;
 
 def Clean():
-    pwm_29.stop()
-    pwm_31.stop()
+    pwm29.stop()
+    pwm31.stop()
     GPIO.cleanup()
     return;
 
 
 
-init()
+#initialize()
 
 for x in range (5):
     Forward(0.2,20)
