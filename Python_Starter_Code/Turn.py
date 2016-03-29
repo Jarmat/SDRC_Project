9e00a6f9beb0b494ec6b772cd585d75a69561288
jammatth <jrm1184@gmail.com>
2016-03-28 19:42:30 -0500

    Small changes.

--- Python_Starter_Code/Turn.py
+++ Python_Starter_Code/Turn.py
@@ -3,6 +3,8 @@ import time
 
 GPIO.setmode(GPIO.BOARD) # select the GPIO numbering scheme to use.
 
+GPIO.setup(18, GPIO.OUT)
+
 pwm_obj = GPIO.PWM(18, 50) #Sets pin 18 as a PWM output at 50 Hz.
 
 pwm_obj.start(100) #Starts the PWM with a duty cycle of 100 (Duty cycle ranges between 0 and 10).
@@ -10,3 +12,21 @@ pwm_obj.start(100) #Starts the PWM with a duty cycle of 100 (Duty cycle ranges b
 pwm_obj.ChangeDutyCycle(50) #Changes the Duty cycle to 50 (obviously).
 
 # Use ChangeDutyCycle() to control the position of the servo in pwm mode.
+
+def TurnLeft(ts):
+    pwm_obj.ChangeDutyCycle(12)
+    time.sleep(ts)
+
+    return;
+
+def TurnRight(ts):
+    pwm_obj.ChangeDutyCycle(4)
+    time.sleep(ts)
+
+    return;
+
+def TurnStraight(ts):
+    pwm_obj.ChangeDutyCycle(8.5)
+
+TunrLeft(0.2)
+TurnRight(0.2)
