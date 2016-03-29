918d684c03f72ccb6f6dfccd6543ecf7c389a303
jammatth <jrm1184@gmail.com>
2016-03-28 20:24:45 -0500

    Added Throttle.py

---
+++ Python_Starter_Code/Throttle.py
@@ -0,0 +1,42 @@
+import RPi.GPIO as GPIO
+import time
+
+def init():
+    GPIO.setmode(GPIO.BOARD) # select the GPIO numbering scheme to use.
+    GPIO.setup(29, GPIO.OUT)
+    GPIO.setup(31, GPIO.OUT)
+    GPIO.setup(33, GPIO.OUT)
+    pwm_29 = GPIO.PWM(29, 100)
+    pwm_31 = GPIO.PWM(31, 100)
+    GPIO.output(33, True)
+
+def Forward(ts,velocity):
+    pwm_31.ChangeDutyCycle(velocity)
+    time.sleep(ts)
+
+
+def Reverse(ts,velocity):
+    pwm_29.ChangeDutyCycle(velocity)
+    time.sleep(ts)
+
+def Stop(ts):
+    GPIO.output(33, False)
+    pwm_29.ChangeDutyCycle(0)
+    pwm_31.ChangeDutyCycle(0)
+
+def Clean():
+    pwm_29.stop()
+    pwm_31.stop()
+    GPIO.cleanup()
+
+init()
+
+for x in range (1:5):
+    Forward(0.2,20)
+
+Stop(0.2)
+
+for x in range(1:5):
+    Reverse(0.2,10)
+
+Clean()
--- Python_Starter_Code/Turn.py
+++ Python_Starter_Code/Turn.py
@@ -1,17 +1,11 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BOARD) # select the GPIO numbering scheme to use.
-
-GPIO.setup(12, GPIO.OUT)
-
-pwm_obj = GPIO.PWM(12, 50) #Sets pin 18 as a PWM output at 50 Hz.
-
-pwm_obj.start(100) #Starts the PWM with a duty cycle of 100 (Duty cycle ranges between 0 and 10).
-
-pwm_obj.ChangeDutyCycle(50) #Changes the Duty cycle to 50 (obviously).
-
-# Use ChangeDutyCycle() to control the position of the servo in pwm mode.
+def init():
+    GPIO.setmode(GPIO.BOARD) # select the GPIO numbering scheme to use.
+    GPIO.setup(12, GPIO.OUT)
+    pwm_obj = GPIO.PWM(12, 50) #Sets pin 18 as a PWM output at 50 Hz.
+    pwm_obj.start(0) #Starts the PWM with a duty cycle of 100 (Duty cycle ranges between 0 and 10).
 
 def TurnLeft(ts):
     pwm_obj.ChangeDutyCycle(12)
@@ -28,5 +22,12 @@ def TurnRight(ts):
 def TurnStraight(ts):
     pwm_obj.ChangeDutyCycle(8.5)
 
-TurnLeft(0.2)
-TurnRight(0.2)
+def Clean():
+    pwm_12.stop()
+    GPIO.cleanup()
+
+TurnLeft(1)
+TurnRight(1)
+TurnStraight(1)
+
+Clean()
