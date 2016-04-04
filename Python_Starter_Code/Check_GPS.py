d6ef6982da32dbf848e5740b9b88744546d200c8
James <jrm1184@gmail.com>
2016-04-04 23:18:56 +0000

    Debug

--- Python_Starter_Code/Check_GPS.py
+++ Python_Starter_Code/Check_GPS.py
@@ -7,9 +7,9 @@ session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
 while True:
     try:
     	report = session.next()
-		# Wait for a 'TPV' report and display the current time
-		# To see all report data, uncomment the line below
-		print report
+	# Wait for a 'TPV' report and display the current time
+	# To see all report data, uncomment the line below
+	print report
         if report['class'] == 'TPV':
             if hasattr(report, 'time'):
                 print report.time
