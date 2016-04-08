11707b0b8ad39b54867ab1ad83d1dbb97d9bcbd0
jammatth <jrm1184@gmail.com>
2016-04-08 15:22:55 -0500

    Debug

--- Python_Starter_Code/PiCamera_Test1.py
+++ Python_Starter_Code/PiCamera_Test1.py
@@ -3,7 +3,8 @@ import picamera
 from Laser import Laser_On, Laser_Off
 
 with picamera.PiCamera() as camera:
-    camera.resolution = (1024, 768)
+    camera.led = False
+    camera.resolution = (640, 480)
     camera.start_preview()
     # Camera warm-up time
     time.sleep(2)
