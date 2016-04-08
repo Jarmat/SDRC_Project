2661f5f2b03ac1bf57e94fe54fe9a3020ccd7bde
jammatth <jrm1184@gmail.com>
2016-04-08 13:52:25 -0500

    Changes to camera stuff

--- Python_Starter_Code/PiCamera_Test_Array.py
+++ Python_Starter_Code/PiCamera_Test_Array.py
@@ -1,12 +1,12 @@
 import time
 import picamera
 import picamera.array
-import cv2
 
 with picamera.PiCamera() as camera:
-    camera.start_preview()
-    time.sleep(2)
-    with picamera.array.PiRGBArray(camera) as stream:
-        camera.capture(stream, format='bgr')
-        # At this point the image is available as stream.array
-        image = stream.array
+    with picamera.array.PiRGBArray(camera) as output:
+        camera.start_preview()
+        time.sleep(2)
+        camera.resolution = (640, 480)
+        camera.capture(output, format='rgb')
+        print('Captured %dx%d image' % (
+                output.array.shape[1], output.array.shape[0]))
