785b2e1c36980a9d41a9d2853efb765f0c1d4bce
jammatth <jrm1184@gmail.com>
2016-04-08 15:52:41 -0500

    Change to numpy array

--- Python_Starter_Code/PiCamera_Test1.py
+++ Python_Starter_Code/PiCamera_Test1.py
@@ -3,22 +3,23 @@ import picamera
 from Laser import Laser_On, Laser_Off
 
 with picamera.PiCamera() as camera:
-    # camera.led = False
-    camera.shutter_speed = 250
-    camera.resolution = (640, 480)
-    camera.start_preview()
-    # Camera warm-up time
-    time.sleep(2)
+    with picamera.array.PiRGBArray(camera) as output:
+        # camera.led = False
+        # camera.shutter_speed = 250
+        camera.resolution = (640, 480)
+        camera.start_preview()
+        # Camera warm-up time
+        time.sleep(2)
 
 
-    try:
-        for x in range(10):
-            start = time.time()
-            Laser_On()
-            camera.capture('foo.jpg')
+        try:
+            for x in range(10):
+                start = time.time()
+                Laser_On()
+                camera.capture(output, format='rgb')
+                Laser_Off()
+                stop = time.time()
+                print stop-start
+        except KeyboardInterrupt:
+            print 'Quit'
             Laser_Off()
-            stop = time.time()
-            print stop-start
-    except KeyboardInterrupt:
-        print 'Quit'
-        Laser_Off()
