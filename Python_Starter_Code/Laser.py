fd144a6a990161aa01683c7f9ba81d09d62cbd2d
jammatth <jrm1184@gmail.com>
2016-04-08 19:35:40 -0500

    Debug

--- Python_Starter_Code/Laser.py
+++ Python_Starter_Code/Laser.py
@@ -22,11 +22,11 @@ def Laser_Off():
     GPIO.output(13,False)
     GPIO.cleanup()
 
-#
-# try:
-#     while True:
-#         Laser_On(0.02)
-#         time.sleep(0.02)
-# except KeyboardInterrupt:
-#     print 'Quit'
-#     GPIO.cleanup()
+
+try:
+    while True:
+        Laser_On(0.02)
+        time.sleep(0.2)
+except KeyboardInterrupt:
+    print 'Quit'
+    GPIO.cleanup()
